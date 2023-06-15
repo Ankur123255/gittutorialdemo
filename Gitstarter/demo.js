798b211f819bfc65b11c67ba47448cedc9115856
Ankur123255 <ankur.chandra66@gmail.com>
2023-06-15 21:43:43 +0530

    demonit

--- Gitstarter/demo.js
+++ Gitstarter/demo.js
@@ -17,15 +17,15 @@
 
 // header.style.borderBottom = 'solid 3px #000';
 // get elemnts by class name
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
 // items[1].textContent = 'hello 2';
-items[0].style.fontWeight = 'bold';
-items[1].style.fontWeight = 'bold';
-items[2].style.fontWeight = 'bold';
-items[3].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'GREEN';
+// items[0].style.fontWeight = 'bold';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'brown';
 // gives error
 // items.style.backgroundColor = '#f4f4f4';
 
@@ -34,3 +34,13 @@ items[2].style.backgroundColor = 'GREEN';
 // }
 
 // GET ELEMENTS BY TAG NAME//
+var li = document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent = 'hello2';
+li[1].style.fontweight = 'bold';
+li[1].style.backgroundColor = 'yellow';
+
+for(var i=0; i<li.length; i++){
+    li[i],style.backgroundColor = '#f4f4f4'
+}
