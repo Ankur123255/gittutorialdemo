e7ba64df3f09274bcb33d1c6d1103eeae37dc9fa
Ankur123255 <ankur.chandra66@gmail.com>
2023-06-21 11:31:31 +0530

    DOM11

---
+++ Gitstarter/DOM11/DOM11.js
@@ -0,0 +1,31 @@
+let form = document.querySelector("form").addEventListener("submit", myfun);
+function myfun(e) {
+  e.preventDefault();
+  let inputName = document.querySelector("#name");
+  let inputEmail = document.querySelector("#email");
+  let inputNumber = document.querySelector("#number");
+
+  if (
+    inputName.value === "" ||
+    inputEmail.value === "" ||
+    inputNumber.value === ""
+  ) {
+    alert("Please Fill All credentials");
+  } else {
+    let userDetails = {
+      Name: inputName.value,
+      Email: inputEmail.value,
+      Phone: inputNumber.value,
+    };
+
+    let storage = JSON.stringify(userDetails);
+    localStorage.setItem("userDetails",storage);
+
+    alert("Registration Done Please Check Local Storage");
+    inputName.value = " ";
+    inputEmail.value = " ";
+    inputNumber.value = " ";
+  }
+
+  console.log(JSON.parse(localStorage.getItem("userDetails")));
+}
